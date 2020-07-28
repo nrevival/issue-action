f3103b72463f94542bf4c6ae95adb49f66bc0f31
Jonghyuck Park @Home <nrevival@gmail.com>
2020-07-28 23:23:34 +0900

    debug

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
 
     // Get the JSON webhook payLoad for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    // console.log(`The event payload: ${payload}`);
 
 } catch (error) {
     core.setFailed(error.message);
